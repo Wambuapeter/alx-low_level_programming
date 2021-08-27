9ed348a81e7169067e7610582ec5c6737b62eb34
Wambuapeter <peterwambua025@gmail.com>
2021-08-27 18:28:26 +0300

    adding 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
