254a118878d06020dcb6682eb558eb1f46280fad
Wambuapeter <peterwambua025@gmail.com>
2021-08-27 17:02:55 +0300

    My first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/**
-*main-the entry point
-*return-always 0(success)
-*/
+/*more headers go here*/
+/*main is the entry point*/
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
+        /*my code goes here*/
         return (0);
+        /*value returned is always 0*/
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,9 @@
 *return-always 0
 */
 int main(void)
+/**
+*main-entry point
+*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
