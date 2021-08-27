b0f74546ca01d1c67eaa27cfa73fdfc5f9413752
Wambuapeter <peterwambua025@gmail.com>
2021-08-27 20:04:06 +0300

    printing the size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 /**
- *main -this code prints the size of various types on the computer
- *Return -always 0
+ *main - this code prints the size of various types on the computer
+ *Return: always 0
  */
 int main(void)
 {
 	 int type_int;
 	 char type_char;
 	 float type_float;
-	 double type_double;
+	 long int type_long;
+	 long long int type_long_long;
+
 	 printf("size of int is:%zu bytes\n", sizeof(type_int));
 	 printf("size of char is:%zu bytes\n", sizeof(type_char));
 	 printf("size of float is:%zu bytes\n", sizeof(type_float));
-	 printf("size of double is:%zu bytes\n", sizeof(type_double));
+	 printf("size of long int is:%zu bytes\n", sizeof(type_long));
+	 printf("size of long long int is:%zu bytes\n", sizeof(type_long_long));
 	return (0);
 }
