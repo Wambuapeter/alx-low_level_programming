b4d7661d85c9e8b5f90184884ba11b6df4dd768f
Wambuapeter <peterwambua025@gmail.com>
2021-08-29 22:23:31 +0300

    last advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main- prints a quote without using printf or puts and returns 1
+ *Return: it is always 0
+ */
+ int main(void)
+ {
+	 write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+	 return (1);
+ }
+ 
