99838f5aaec9b8a1be03a9e760cd7ab94ad5a9e7
Wambuapeter <peterwambua025@gmail.com>
2021-08-29 22:32:39 +0300

    last advanced task done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  *main- prints a quote without using printf or puts and returns 1
  *Return: it is always 0
  */
- int main(void)
- {
-	 write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
-	 return (1);
- }
- 
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
