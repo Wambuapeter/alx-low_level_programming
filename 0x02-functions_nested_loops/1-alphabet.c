fb9c1cb2f89bf9622500304a9dc694bf56cac34e
Wambuapeter <peterwambua025@gmail.com>
2021-09-01 08:54:14 +0300

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *print_alphabet- prints the alphabet
+ *Return: always o
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a', x <= 'z', x++)
+	{
+		putchar(x);
+		putchar(\n);
+	}
+}
