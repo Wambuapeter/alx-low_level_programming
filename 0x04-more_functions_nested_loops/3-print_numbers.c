d017799fa6b9ab563fd327c42a65b756f994df3c
Wambuapeter <peterwambua025@gmail.com>
2021-09-03 14:59:41 +0300

    correcting errors

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,6 +7,6 @@ void print_numbers(void)
 	char c;
 
 	for (c = 48; c < 58; c++)
-	_putchar(c);
-	_putchar('\n');
+	putchar(c);
+	putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,8 @@ void print_most_numbers(void)
 	for (c = 48; c < 58; c++)
 	{
 		if (c != 50 && c != 52)
-		_putchar(c);
+		putchar(c);
 
 	}
-	_putchar('\n');
+	putchar('\n');
 }
