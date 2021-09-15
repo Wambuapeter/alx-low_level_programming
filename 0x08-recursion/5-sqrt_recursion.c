c2bc821c47bf1ad72e9ee6f35e50ac9b5231e5bb
Wambuapeter <peterwambua025@gmail.com>
2021-09-15 21:47:02 +0300

    return natural square of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * auxfunction - natural square root of a number
+ * @n: number
+ * @i: multiple
+ * Return: o,though -1 if n has no natural square
+ */
+int auxfunction(int n, int i)
+{
+	if (n == (i * i))
+	{
+		return (i);
+	}
+	else if (n > (i * i))
+	{
+		return (auxfunction(n, i + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: number
+ * Return: o,though -1 if n has no natural square
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (auxfunction(n, 1));
+}
