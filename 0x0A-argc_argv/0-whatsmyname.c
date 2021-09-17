58133f926218c09a9b662cb01f58d3ed824a1b4e
Wambuapeter <peterwambua025@gmail.com>
2021-09-17 14:32:50 +0300

    what's the programs name?

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argc: no of command line arguments
  * @argv: array of size argc
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
