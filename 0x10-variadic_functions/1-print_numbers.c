3fa83b11b361bbbbecd760423c1f84ab6b82c54c
Wambuapeter <peterwambua025@gmail.com>
2021-09-30 18:32:50 +0300

    print numbers\n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,9 +8,9 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list(ap);
+	unsigned int i;
 
-	int i;
+	va_list(ap);
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
