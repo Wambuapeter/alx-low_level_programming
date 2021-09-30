3cae273335afaeabe384d19c0fa40174d2c7fa57
Wambuapeter <peterwambua025@gmail.com>
2021-09-30 18:48:40 +0300

    print strings\n

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
-		printf("\n");
-		va_end(ap);
 	}
+	printf("\n");
+	va_end(ap);
 }
