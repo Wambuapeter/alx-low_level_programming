74113d9cf4f7d0fe9175dbadc15b7abc4b47f947
Wambuapeter <peterwambua025@gmail.com>
2021-09-30 11:59:49 +0300

    print numbers\n

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: string printed bbtn nos
+ * @n: number of integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(ap);
+
+	int i;
+
+	va_start(ap, n);
+	if (n == 0)
+	{
+		printf("\n");
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf("%d%s", va_arg(ap, int), separator);
+		}
+	}
+	printf("%d\n", va_arg(ap, int));
+	va_end(ap);
+}
+
