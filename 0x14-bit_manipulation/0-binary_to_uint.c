c801e67f2dbbb802a8f0cbbd4cc4171e2cd0ee38
Wambuapeter <peterwambua025@gmail.com>
2021-10-14 15:34:21 +0300

    converting binary into unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary no. to unsigned int
+ * @b: char pointing to a string of 0 and 1 chars
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != 0 || b[i] != 1)
+			return (0);
+		dec = 2 * dec + (b[i] - '0');
+	}
+	return (dec);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+I love numbers!This certainly makes bit manipulation one of my best parts of programming.Let's dive into some code
