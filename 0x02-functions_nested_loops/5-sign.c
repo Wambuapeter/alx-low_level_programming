2b0676f570fb9720a17b080e7ca1dbd73cc7a864
Wambuapeter <peterwambua025@gmail.com>
2021-11-08 19:33:19 +0300

    check the sign mr programmer

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -10,18 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
 		return (1);
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		putchar('0');
 		return (0);
+		_putchar('0');
 	}
 	else if (n < 0)
 	{
-		putchar('-');
 		return (-1);
+		_putchar('-');
 	}
 	return (0);
 }
